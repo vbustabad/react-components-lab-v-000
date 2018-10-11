acd886759fb5c7321840719c45795c10d7aa8bb2
Veronica Bustabad <vbustabad@gmail.com>
2018-10-11 22:22:56 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import React, { Component } from 'react' // We need to import react so we can make use of its .component class
-// import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
+import React, { Component } from 'react' // We need to import react so we can make use of its .component class
+import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
 
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
@@ -8,24 +8,63 @@ const BUTCHER_PRODUCTS = [
   'Ribeye'
 ]
 
+const products = [<li>{BUTCHER_PRODUCTS[0]}</li>, <li>{BUTCHER_PRODUCTS[1]}</li>, <li>{BUTCHER_PRODUCTS[2]}</li>, <li>{BUTCHER_PRODUCTS[3]}</li>]
+
 // Define these exported classes
 export class OlderCoaster extends Component {
 
   // this function should return the JSX we want the DOM to render
-  // render() {
-  //   return (
-  //     <div>Hello!</div>
-  //   )
-  // }
-  //
+  render() {
+    return (
+      <div class="oldercoaster">
+        <p>
+          Two grannies having the time of their life!
+        </p>
+        <p>
+          Passengers:
+        </p>
+        <ul>
+          <li>
+            Agnes
+          </li>
+          <li>
+            Muriel
+          </li>
+        </ul>
+      </div>
+    )
+  }
+
 }
 
 export class InFrontOfYou extends Component {
-  // your code here
+  render() {
+    return (
+      <div>
+        <p>
+          You shouldn't look too far.
+        </p>
+        <p>
+          Sometimes, the solution is right in front of you.
+        </p>
+      </div>
+    )
+  }
 }
 
 export class ButcherShop extends Component {
-  // your code here
+  render() {
+    return (
+      <div class="butcher-shop">
+        <p>
+          Hello! We have the following products for sale today:
+        </p>
+        <ul>
+          { products }
+        </ul>
+      </div>
+    )
+  }
 }
 
 
@@ -33,7 +72,9 @@ export class App extends Component {
   render() {
     return (
       <div id="app">
-
+        <OlderCoaster />
+        <InFrontOfYou />
+        <ButcherShop />
       </div>
     )
   }
